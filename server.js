12b3c8c1f12f0b4029167f7495f721520d38d1be
PoffDev <joshuapoffenberger@gmail.com>
2017-10-04 16:01:00 -0400

    server.js up and running

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mysql = require('mysql');
+const bodyParser = require('body-parser')
+
+const app = express();
+const port = process.env. PORT || 8000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.text());
+app.use(bodyParser.json({
+    type: "application/vnd.api+json"
+}));
+
+app.listen(port, function(){
+    console.log('app listening on port ' + port)
+})
