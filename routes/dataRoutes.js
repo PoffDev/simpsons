45cd7c63420073d6c959c1c56f977f347cc307bf
PoffDev <joshuapoffenberger@gmail.com>
2017-10-04 17:12:55 -0400

    can pull sql data on route hit

--- routes/dataRoutes.js
+++ routes/dataRoutes.js
@@ -1,12 +1,34 @@
 const path = require('path');
 const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+    user: 'root',
+    password: 'root',
+    database: 'Simpsons'
+});
+
+connection.connect( function(error){
+    if (error) throw err
+
+    console.log('mySQL connected as id: ' + connection.threadId);
+
+    // connection.query("SELECT * FROM `Episodes` WHERE ?", { season: 2 }, function(err, results){
+
+    //     console.log('Got results', results)
+    // });
+})
+
 module.exports = function(app){
 
-    app.get('/search', function(req, res){
+    app.get('/season/:number', function(req, res){
 
-        res.send(
-            console.log('hello from /search')
-        )
+        let seasonSearched = req.params.number;
+        console.log('season selected: '+ seasonSearched)
+
+        connection.query("SELECT * FROM `Episodes` WHERE ?", {
+            season: seasonSearched,
+        }, function(error, response){
+            console.log('results', response);
+        })
     })
 }
--- server.js
+++ server.js
@@ -1,18 +1,19 @@
-const express = require('express');
 const mysql = require('mysql');
 const bodyParser = require('body-parser')
+const express = require('express');
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+    app.use(bodyParser.text());
+    app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+
+const exphbs = require("express-handlebars");
+    app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+    app.set("view engine", "handlebars");
 
 const app = express();
 const port = process.env. PORT || 8000;
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({
-    type: "application/vnd.api+json"
-}));
+
 
 require('./routes/dataRoutes.js')(app);
 require('./routes/htmlRoutes.js')(app);
