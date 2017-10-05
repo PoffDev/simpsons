335fea6089af96832e59772739cb6b40fe47354a
PoffDev <joshuapoffenberger@gmail.com>
2017-10-05 12:00:56 -0400

    able to display json on expanded click

--- app/routes/dataRoutes.js
+++ app/routes/dataRoutes.js
@@ -21,9 +21,11 @@ module.exports = function(app){
     })
 
     app.get('/season/:number', function(req, res){
-
         let seasonSearched = req.params.number;
-        connection.query("SELECT * FROM `Episodes` WHERE ? ORDER BY `number_in_season`", { season: seasonSearched}, function(error, response){
+
+        connection.query("SELECT * FROM Episodes WHERE ? ORDER BY `number_in_season`", { 
+            season: seasonSearched
+        }, function(error, response){
 
             if (error) throw error;
             res.render("index", {
@@ -32,4 +34,19 @@ module.exports = function(app){
 
         })
     })
-}
+
+    app.get('/season/:number/:productionCode', function (req, res){
+        let seasonSearched = req.params.number;
+        let codeSearched = req.params.productionCode;
+
+        connection.query("SELECT * FROM Episodes WHERE `season`= ? AND `production_code`= ?;", 
+            [seasonSearched, codeSearched], function(error, response){
+
+                if (error) throw error;
+
+                res.json(response);
+            }
+        );
+
+    })
+};
